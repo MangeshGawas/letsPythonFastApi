10de4adecac83418100a1ffda13b806e8e76f90c
Mangesh Gawas <manikgawas07@gmail.com>
2026-09-03 09:22:00 +0530

    list and its mostly used function

---
+++ List.py
@@ -0,0 +1,27 @@
+my_list = [12,3,54,65,23,1]
+print(my_list[0])
+print(my_list[3])
+print(my_list)
+
+
+people_list = ["Sam", "Man","halland"]
+print(people_list)
+people_list[0] ="Mangesh"
+print(len(people_list))
+print(people_list)
+
+#slicing
+print(people_list[0:2])
+
+#append
+my_list.append(1000)
+print(my_list)
+#insert
+my_list.insert(2,100)
+print(my_list)
+#remove
+my_list.remove(23)
+print(my_list)
+#pop
+my_list.pop(3)
+print(my_list)
