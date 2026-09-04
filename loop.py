5d6bf2d31a161cc245a41955698e33bef6cc552e
Mangesh Gawas <manikgawas07@gmail.com>
2026-09-04 11:57:35 +0530

    forLoop

---
+++ loop.py
@@ -0,0 +1,16 @@
+my_list = [1,2,3,4,5,6]
+for i in range(len(my_list)):
+    print(my_list[i])
+
+print("2nd logic")
+for i in my_list:
+    print(i)
+
+#sum of list no
+
+sum = 0
+for i in my_list:
+    sum +=i
+    # print(sum)
+
+print(sum)
