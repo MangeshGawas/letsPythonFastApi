36b6a8d1030356d71f999fe6d9ee0c953e500ad9
Mangesh Gawas <manikgawas07@gmail.com>
2026-09-04 11:32:55 +0530

    assignment4

--- assignment4.py
+++ assignment4.py
@@ -15,3 +15,18 @@ D = 60 - 69
 
 F = 0 - 59
 '''
+
+grade = 80
+
+grade = 80
+
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("F")
