5cdc64d7ac3e64b351afc9df1746edae4d40f043
Mangesh Gawas <manikgawas07@gmail.com>
2026-09-04 11:09:40 +0530

    operators and conditional statement

---
+++ BooleanAndOperator.py
@@ -0,0 +1,26 @@
+'''
+Boolean ans Operator
+'''
+
+like_coffee = True
+like_tea = True
+
+print(type(like_coffee))
+
+#operator
+
+print(1==2)
+
+print(1!=2)
+
+print(1>2)
+
+print(1<2)
+
+print(1<=1)
+
+#logical Operator
+
+print(1>3 and 5<7)
+print(1<3 or 5<7)
+print(1==1)
---
+++ ifElse.py
@@ -0,0 +1,15 @@
+x = 1
+# x = 2
+if x==1:
+    print("x is 1")
+else:
+    print('x is not 1')
+
+
+hour = 17
+if hour<12:
+    print("Good Morning")
+elif hour <20:
+    print("Good Afternoon")
+else:
+    print("Good Night")
