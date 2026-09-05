3dc9d7e46de84665d2695389a09212953763f7fa
Mangesh Gawas <manikgawas07@gmail.com>
2026-09-05 09:10:42 +0530

    loop while loop in that continue and break

--- loop.py
+++ loop.py
@@ -14,3 +14,17 @@ for i in my_list:
     # print(sum)
 
 print(sum)
+
+
+#while loop
+
+i = 0
+while i<5:
+    i +=1
+    if i ==3:
+        continue
+    print(i)
+    if i ==4:
+        break
+else:
+    print("i is now larger or equal to 5")
