a8def83f4b8a114c4cbd9bdb32a80e7419241775
Mangesh Gawas <manikgawas07@gmail.com>
2026-09-06 21:11:59 +0530

    functions in python

---
+++ functions.py
@@ -0,0 +1,13 @@
+print("hello")
+
+
+def my_function():
+    print("this is my new function")
+
+my_function()
+
+def my_function2(typ):
+    print(f"this is my {typ} function")
+
+my_function2("argumented")
+
